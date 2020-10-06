afa2ecf2fbb8bca43d0a8f107a494b049531a80d
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-10-06 10:57:16 -0600

    1.04.1 Integración de Bootstrap
    - Se intregra bootstrap como librería CSS al proyecto.

---
+++ src/public/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Préstamo de libros - CETis108</title>
+  
+  <!-- Hojas de estilo CSS -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand">CETis108</a>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-4">CETis108 Sistema Bibliotecario📚</h1>
+      <p class="lead">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt totam sed velit a alias rerum officiis, eveniet magnam eaque reiciendis dicta, dolore, fuga nihil quibusdam qui aut ab iusto consequuntur.
+      </p>
+      <hr>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores consectetur quibusdam laboriosam dignissimos? Adipisci, quasi dolorem!
+      </p>
+      <button class="btn btn-primary btn-lg">Iniciar sesión 🔐</button>
+    </div>
+  </div>
+
+  <!-- Script Javascript -->
+  <script src="js/jquery-3.5.1.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.js"></script>
+</body>
+</html>
