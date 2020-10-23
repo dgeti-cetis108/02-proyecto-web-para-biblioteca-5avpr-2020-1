4e0320520c7d9f09f5e99129476d86ab3e9d177a
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-10-23 09:05:45 -0600

    1.08 Resultados de la búsqueda
    Media object
    Pagination

--- src/public/busqueda.php
+++ src/public/busqueda.php
@@ -1,12 +1,65 @@
 <?php require_once '../app/layouts/header.php'; ?>
 
 <div class="container">
-  <form style="max-width: 600px;margin-right: auto;margin-left: auto;">
+  <form class="login-form">
     <div class="input-group">
         <div class="input-group-prepend"></div><input class="form-control" type="text" placeholder="Ingresa el nombre del libro o el autor" style="border-top-left-radius: 4px;border-bottom-left-radius: 4px;">
-        <div class="input-group-append"><button class="btn btn-primary" type="button" style="background: rgb(160,55,85);"><i class="fa fa-search"></i></button></div>
+        <div class="input-group-append">
+          <button class="btn btn-primary btn-main" type="button">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
     </div>
   </form>
+</div>
+
+<div class="container">
+  <div class="media mb-4"><img class="mr-3" style="width:100px; height:120px;" />
+    <div class="media-body">
+      <h5>Media Heading</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus nisl ac diam feugiat, non vestibulum libero posuere. Vivamus pharetra leo non nulla egestas, nec malesuada orci finibus. </p>
+    </div>
+  </div>
+  <div class="media mb-4"><img class="mr-3" style="width:100px; height:120px;" />
+    <div class="media-body">
+      <h5>Media Heading</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus nisl ac diam feugiat, non vestibulum libero posuere. Vivamus pharetra leo non nulla egestas, nec malesuada orci finibus. </p>
+    </div>
+  </div>
+  <div class="media mb-4"><img class="mr-3" style="width:100px; height:120px;" />
+    <div class="media-body">
+      <h5>Media Heading</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus nisl ac diam feugiat, non vestibulum libero posuere. Vivamus pharetra leo non nulla egestas, nec malesuada orci finibus. </p>
+    </div>
+  </div>
+  <div class="media mb-4"><img class="mr-3" style="width:100px; height:120px;" />
+    <div class="media-body">
+      <h5>Media Heading</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus nisl ac diam feugiat, non vestibulum libero posuere. Vivamus pharetra leo non nulla egestas, nec malesuada orci finibus. </p>
+    </div>
+  </div>
+  <div class="media mb-4"><img class="mr-3" style="width:100px; height:120px;" />
+    <div class="media-body">
+      <h5>Media Heading</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus nisl ac diam feugiat, non vestibulum libero posuere. Vivamus pharetra leo non nulla egestas, nec malesuada orci finibus. </p>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div style="text-align:center;">
+    <nav>
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">4</a></li>
+        <li class="page-item"><a class="page-link" href="#">5</a></li>
+        <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+      </ul>
+    </nav>
+  </div>
 </div>
 
 <?php require_once '../app/layouts/footer.php'; ?>
